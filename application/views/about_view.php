dbcb25e8096e1195c073dd2704edc1f963b8b1b9
ivonfr <ivon.rahma@gmail.com>
2017-03-02 16:39:43 +0700

    Commit Pertama

---
+++ application/views/about_view.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>WorkShop</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<img src="https://cdn.pixabay.com/photo/2016/10/15/15/49/workshop-1742721__340.jpg " width="100%" height="100%" alt="">
+			</div>
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					<nav class="navbar navbar-default" role="navigation">
+						<div class="container-fluid">
+							<!-- Brand and toggle get grouped for better mobile display -->
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+								<a class="navbar-brand" href="#">WorkShop</a>
+							</div>
+					
+							<!-- Collect the nav links, forms, and other content for toggling -->
+							<div class="collapse navbar-collapse navbar-ex1-collapse">
+								<ul class="nav navbar-nav">
+									<li ><a href="http://localhost/CodeIgniter/index.php/welcome/index">Home</a></li>
+									<li class="active"><a href="#">Abaout</a></li>
+									<li><a href="http://localhost/CodeIgniter/index.php/welcome/contact">Contact</a></li>
+								</ul>
+								<form class="navbar-form navbar-left" role="search">
+									<div class="form-group">
+										<input type="text" class="form-control" placeholder="Search">
+									</div>
+									<button type="submit" class="btn btn-default">Cari</button>
+								</form>
+								<ul class="nav navbar-nav navbar-right">
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown">Logout <b class="caret"></b></a>
+										<ul class="dropdown-menu">
+											<li><a href="#">Logout</a></li>
+											<li><a href="#">Sig Up</a></li>
+										</ul>
+									</li>
+								</ul>
+							</div><!-- /.navbar-collapse -->
+						</div>
+					</nav>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					<div class="jumbotron">
+				
+					<center>
+									<table  cellpadding="0px" cellspacing="0px" border="0px" align="left">
+										<tr>
+											<td>
+												<img src="https://cdn.pixabay.com/photo/2013/07/12/17/48/girl-152498_640.png" width="160px"  height="200px" align="navbar-right">
+											</td>
+											<td></td>
+											<td>
+											<p align="navbar-right">
+												<h3> <font face="Freestyle Script">Ivon Fadhila Rahma anak ke 2 dari dua bersaudara . </font></h3>
+												</p>
+											</td>
+										</tr>
+									</table>
+										<br/>
+										<br/><br/><br/><br/><br/>
+									<table width="600px"  height="100px" cellpadding="0px" cellspacing="0px" border="0px" align="navbar-right">
+										<tr>
+
+											<td>
+												<h3> <font face="Freestyle Script">berasal dari kota probolinggo, dan kuliah di Politeknik Negeri Malang Jurusan Teknologi Informasi Prodi D4-Teknik Informatika. nim 1541180021. </font></h3>
+											</td>
+											<td>
+												<img src="http://alfahmu.com/wp-content/uploads/2014/11/kartun-rumah-Undanglah-orang-beriman-ke-rumah.png" width="200px"  height="240px" align="navbar-right">
+											</td>
+										</tr>
+									</table>
+									<br/>
+										<br/>
+										<table  cellpadding="0px" cellspacing="0px" border="0px" align="left">
+										<tr>
+											<td>
+												<img src="http://smartmomways.com/wp-content/uploads/2014/06/maskot-for-PNG.png" width="200px"  height="160px" align="navbar-right">
+											</td>
+											<td></td>
+											<td>
+												<h3><font face="Freestyle Script">Hobinya main, jalan-jalan, suka ngoding juga kok hehe.. nomor hp 089631988015</font></h3>
+											</td>
+										</tr>
+									</table>
+										<br/>
+										
+									
+									</center>
+						
+					</div>
+				</div>
+			</div>
+
+			
+		</div>
+		
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="Hello World"></script>
+	</body>
+</html>
---
+++ application/views/home_view.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>WorkShop</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<img src="https://cdn.pixabay.com/photo/2016/10/15/15/49/workshop-1742721__340.jpg " width="100%" height="100%" alt="">
+			</div>
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					<nav class="navbar navbar-default" role="navigation">
+						<div class="container-fluid">
+							<!-- Brand and toggle get grouped for better mobile display -->
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+								<a class="navbar-brand" href="#">WorkShop</a>
+							</div>
+					
+							<!-- Collect the nav links, forms, and other content for toggling -->
+							<div class="collapse navbar-collapse navbar-ex1-collapse">
+								<ul class="nav navbar-nav">
+									<li class="active"><a href="#">Home</a></li>
+									<li><a href="http://localhost/CodeIgniter/index.php/welcome/about">Abaout</a></li>
+									<li><a href="http://localhost/CodeIgniter/index.php/welcome/contact">Contact</a></li>
+								</ul>
+								<form class="navbar-form navbar-left" role="search">
+									<div class="form-group">
+										<input type="text" class="form-control" placeholder="Search">
+									</div>
+									<button type="submit" class="btn btn-default">Cari</button>
+								</form>
+								<ul class="nav navbar-nav navbar-right">
+									<li class="dropdown">
+										<a href="#" class="dropdown-toggle" data-toggle="dropdown">Logout <b class="caret"></b></a>
+										<ul class="dropdown-menu">
+											<li><a href="#">Logout</a></li>
+											<li><a href="#">Sig Up</a></li>
+										</ul>
+									</li>
+								</ul>
+							</div><!-- /.navbar-collapse -->
+						</div>
+					</nav>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+					<div class="jumbotron">
+						<div class="container">
+						<center><img src="https://lh4.googleusercontent.com/-lF59liFfI8Q/AAAAAAAAAAI/AAAAAAAAAKY/FB-V0jodZcs/photo.jpg " width="150px" height="150px" alt="Cinque Tere" class="img-circle"></center>
+							<h1><center><font face="Broadway">Welcome to Work Shop Riset Informatika </font></center></h1>
+							<h3><center><center><font face="Blackadder ITC">with </font></center></h3>
+							
+							<h1><center><b><font face="BAharoni">Ivon Fadhila Rahma </font><b></center></h1>
+							<br/>
+							<p>
+								<center><a href="http://localhost/CodeIgniter/index.php/welcome/about" class="btn btn-primary btn-lg">Learn more</a></center>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			
+		</div>
+		
+
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 		<script src="Hello World"></script>
+	</body>
+</html>
